5e68a3d7788f680f661dd45fa7a9f6dbeaf7a4ad
Ballsky22 <ballskyteam22@gmail.com>
2023-01-31 21:08:37 +0700

    Update Slider

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
+// Sidebar
 const sideNav = document.querySelectorAll(".sidenav");
 M.Sidenav.init(sideNav);
+
+// Slider
+
+const slider = document.querySelectorAll('.slider');
+M.Slider.init(slider, {
+    indicators: true,
+    height: 650,
+    transition: 600,
+    interval: 3000
+});
