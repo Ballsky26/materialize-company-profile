25c710872e98b6a634579bbb30bbf9a731a81194
Ballsky22 <ballskyteam22@gmail.com>
2023-01-31 22:28:21 +0700

    Update Card

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ const sideNav = document.querySelectorAll(".sidenav");
 M.Sidenav.init(sideNav);
 
 // Slider
-
 const slider = document.querySelectorAll('.slider');
 M.Slider.init(slider, {
     indicators: true,
@@ -11,3 +10,7 @@ M.Slider.init(slider, {
     transition: 600,
     interval: 3000
 });
+
+// Parallax
+const parallax = document.querySelectorAll('.parallax');
+M.Parallax.init(parallax);
